e5c7c184e648c3fd1a5b8deef8619dcb01fd48c1
Rodrigo Santos <009rodrigo009@gmail.com>
2024-03-09 14:17:25 -0300

    updated scrollbutton

--- src/components/ScrollButton/index.tsx
+++ src/components/ScrollButton/index.tsx
@@ -1,12 +1,11 @@
-import { ScrollButtonA } from "./styles";
 import { FaArrowUp } from "react-icons/fa";
 
 export function ScrollButton() {
   return (
-    <ScrollButtonA>
-      <a style={{ position: "fixed", bottom: 30, right: "10%", }} href="#">
+    <button>
+      <a style={{ position: "fixed", bottom: 30, right: "10%" }} href="#">
         <FaArrowUp style={{ fontSize: 30 }} />
       </a>
-    </ScrollButtonA>
+    </button>
   );
 }
